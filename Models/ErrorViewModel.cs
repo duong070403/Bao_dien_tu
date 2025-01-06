eccdded6a9941735f5518786244e37dd7706a3f8
duong070403 <obuccaminhdaykho@gmail.com>
2025-01-06 16:51:06 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTL_Xay_dung_bao_dien_tu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
